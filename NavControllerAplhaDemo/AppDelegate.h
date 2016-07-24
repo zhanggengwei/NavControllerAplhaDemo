77319bc49515a2b20e372b582c406e1195fd3dda
zhanggeng <m13716920635@163.com>
2016-07-25 01:55:25 +0800

    Initial Commit

---
+++ NavControllerAplhaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavControllerAplhaDemo
+//
+//  Created by vd on 16/7/25.
+//  Copyright © 2016年 vd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
